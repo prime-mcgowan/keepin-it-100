c103a3b4d3ccf0da41b8b32950c8438207b0ff0c
Laura McGowan <prime.lmcg@gmail.com>
2023-04-24 14:56:15 -0500

    code page title, instructions and number inputs coded and working

--- js/site.js
+++ js/site.js
@@ -1,3 +1,15 @@
-function helloWorld() {
-    alert("Hello World!!");
+
+// get values form the inputs
+function getValues() {
+    let startValue = document.getElementById
+}
+
+//generate numbers from startValue to endValue
+function generateNumbers() {
+
+}
+
+//display numbers and mark even numbers bold
+function displayNumbers() {
+
 }
