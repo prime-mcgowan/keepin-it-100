25bbd0e380d3ca9cb7284eb8676f7351d88abfea
Laura McGowan <prime.lmcg@gmail.com>
2023-04-24 15:37:17 -0500

    functions for: getting values and generating numbers written

--- js/site.js
+++ js/site.js
@@ -1,12 +1,20 @@
 
 // get values form the inputs
 function getValues() {
-    let startValue = document.getElementById
+    let startValue = document.getElementById("startValue").value
+    let endValue = document.getElementById("endValue").value
 }
 
 //generate numbers from startValue to endValue
-function generateNumbers() {
+let numbers = generateNumbers(startValue, endValue);
 
+function generateNumbers(sValue, eValue){
+    let numbers = [];
+    for(let i=sValue; i<=eValue; i++){
+        numbers.push(index);
+    }
+    
+    return numbers;
 }
 
 //display numbers and mark even numbers bold
